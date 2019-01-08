ca9358742a28c245d139bd5d036ca477af125aba
Cody James Casey <codyjamescasey@gmail.com>
2019-01-07 19:21:18 -0500

    matt-smash

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { split } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { WebSocketLink } from "apollo-link-ws";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { getMainDefinition } from "apollo-utilities";
+
+const httpLink = new HttpLink({
+  uri: "https://api.graph.cool/simple/v1/cjqmyjr8y9q1w0154qgcv8wpi"
+});
+
+const wsLink = new WebSocketLink({
+  uri: "wss://subscriptions.us-west-2.graph.cool/v1/cjqmyjr8y9q1w0154qgcv8wpi",
+  options: {
+    reconnect: true
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
